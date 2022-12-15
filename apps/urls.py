ab4100e1737c03f37b3f94bd43dfea6c02f016ce
jak010 <bluetoon@naver.com>
2022-12-15 16:44:18 +0900

    <fix> 미사용 파일 제거

--- apps/urls.py
+++ apps/urls.py
@@ -1,15 +1,13 @@
 # from django.conf.urls import url  # django 4.x 버전 부터는 deprecated 처리
 
-from django.urls import include, path
+from django.urls import path
 
 from .views import (
-    greeting,
     member_api,
     email_send_api
 )
 
 urlpatterns = [
-    path("", greeting.GreetingView.as_view()),
 
     # Member
     path("member", member_api.MemberCreateView.as_view()),
