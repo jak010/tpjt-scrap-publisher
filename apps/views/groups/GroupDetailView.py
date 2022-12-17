a13ecaebf0c3655ffefa483edd14b11062f1f48a
jak010 <bluetoon@naver.com>
2022-12-17 13:03:53 +0900

    <feat> GroupDetailView에 SubScribe 정보 추가

--- apps/views/groups/GroupDetailView.py
+++ apps/views/groups/GroupDetailView.py
@@ -9,12 +9,18 @@ class GroupDetailView(View):
         group_id = int(self.kwargs['group_id'])
 
         group = Group.objects.get(id=group_id)
+        group_members = group.user_set.all()
+        group_subscribes = group.groupsubscribe_set.all()
 
         return JsonResponse(status=200, data={
             'group_id': group.id,
             'group_name': group.name,
-            'member': [{
+            'members': [{
                 'member_id': member.id,
                 'email': member.email
-            } for member in group.user_set.all()]
+            } for member in group_members],
+            'subscribes': [{
+                'reference_id': subscribe.pk,
+                'url': subscribe.url
+            } for subscribe in group_subscribes]
         })
