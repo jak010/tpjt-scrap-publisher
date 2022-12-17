d4a9eebbe69289447d0d694b73874031715edc65
jak010 <bluetoon@naver.com>
2022-12-17 12:46:39 +0900

    <feat> 초기 데이터 생성 BaseCommand 추가

---
+++ apps/management/commands/app_setup.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import Group
+from ...orm import Member
+
+
+class Command(BaseCommand):
+    help = "초기 데이터 생성"
+
+    COMMON_EMAILS = ["test01@test.com", "test02@test.com", "test03@test.com"]
+    COMMON_PASSWORD = "1234"
+
+    GROUP_NAME = "test"
+
+    def add_arguments(self, parser):
+        parser.add_argument("init", type=str)
+
+    def handle(self, *args, **options):
+        flag = options['init']
+
+        if flag == 'all':
+            self.make_member()
+            self.make_group()
+            self.make_group_member()
+
+    def make_member(self):
+        for email in self.COMMON_EMAILS:
+            new_member01 = Member(email=email)
+            new_member01.set_password(self.COMMON_PASSWORD)
+            new_member01.save()
+
+    def make_group(self):
+        g = Group(name=self.GROUP_NAME)
+        g.save()
+
+    def make_group_member(self):
+        g = Group.objects.get(name="test")
+
+        for email in self.COMMON_EMAILS:
+            m = Member.objects.get(email=email)
+            m.groups.add(g)
