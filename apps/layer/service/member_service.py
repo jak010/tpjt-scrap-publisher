28bfc39fb0fb27a16074723a86d0cbafc836cd39
jak010 <bluetoon@naver.com>
2022-12-28 11:52:38 +0900

    <refactor> member_login_service, class -> function으로 변경

--- apps/layer/service/member_service.py
+++ apps/layer/service/member_service.py
@@ -4,34 +4,21 @@ from django.contrib.sessions.backends.db import SessionStore
 from ..exceptions.member_exceptions import MemberAuthenticateFailError
 
 
-class MemberAuthenticateService:
+def member_authenticate(request, login_email: str, login_password: str):
+    """ 사용자 인증하기 """
+    auth = authenticate(
+        username=login_email,
+        password=login_password
+    )
 
-    def __init__(self, login_email: str, login_password: str):
-        self._login_email = login_email
-        self._login_password = login_password
+    print(auth)
+    if auth is None: raise MemberAuthenticateFailError()
 
-    def check_authenticate(self):
-        auth = authenticate(
-            username=self._login_email,
-            password=self._login_password
-        )
+    login(request, auth)
 
-        if auth is None: raise MemberAuthenticateFailError
-
-        return auth
+    return auth
 
 
-class MemberLoginService:
-
-    @classmethod
-    def login(cls, request, auth):
-        login(request, auth)
-        return cls(request, auth)
-
-    def __init__(self, request, auth):
-        self._request = request
-        self.auth = auth
-
-    @property
-    def session(self) -> SessionStore:
-        return SessionStore(session_key=self._request.session.session_key)
+def member_session(request, auth) -> SessionStore:
+    """ 사용자 session 찾기 """
+    return SessionStore(session_key=request.session.session_key)
--- apps/views/members/member_login.py
+++ apps/views/members/member_login.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 
-from typing import Union
-
 import backoff as check
 from django.http import JsonResponse
 from django.views import View
 
 from apps.layer.exceptions import member_exceptions
-from apps.layer.service.member_service import MemberAuthenticateService, MemberLoginService
+from apps.layer.service import member_service
 from .dto import MemberLoginFormDto
 
 
@@ -25,20 +23,18 @@ class MemberLoginView(View):
         member_login_form_dto = MemberLoginFormDto(self.request.POST)
         member_login_form_dto.is_valid()
 
-        member_authenticate_service: Union[
-            MemberAuthenticateService,
-            member_exceptions.MemberAuthenticateFailError
-        ] = MemberAuthenticateService(
+        member_authenticate = member_service.member_authenticate(
+            request=self.request,
             login_email=member_login_form_dto.get_email,
             login_password=member_login_form_dto.get_password
         )
 
-        member_login_service = MemberLoginService.login(
+        member_session = member_service.member_session(
             request=self.request,
-            auth=member_authenticate_service.check_authenticate()
+            auth=member_authenticate
         )
 
         return JsonResponse(status=200, data={
-            'session_key': member_login_service.session.session_key,
-            "expire_date": member_login_service.session.get_expiry_date()
+            'session_key': member_session.session_key,
+            "expire_date": member_session.get_expiry_date()
         })
